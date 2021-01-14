5947eca0dd9d3320858547058492a7363d757918
Irene Asay <irene.hsu.asay@gmail.com>
2021-01-14 10:30:55 +0800

    updated the src files and refactored app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
-import React from "react";
-import Counter from "./components/Counter";
+import React, { Component } from 'react';
+import FriendCard from './components/FriendCard';
+import Wrapper from './components/Wrapper';
+import Title from './components/Title';
+import friends from './friends.json';
 
-function App() {
-  return <Counter />;
+
+class App extends Component {
+  render() {
+    return (
+      <Wrapper>
+        <Title>Friends List</Title>
+        <FriendCard
+          this.state.
+        />
+        <FriendCard
+          name={friends[1].name}
+          image={friends[1].image}
+          occupation={friends[1].occupation}
+          location={friends[1].location}
+        />
+        <FriendCard
+          name={friends[2].name}
+          image={friends[2].image}
+          occupation={friends[2].occupation}
+          location={friends[2].location}
+        />
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
+
