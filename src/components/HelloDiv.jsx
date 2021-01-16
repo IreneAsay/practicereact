518d7e4908a1c6ae72eff7b69370a7b3baf9b094
Irene Asay <irene.hsu.asay@gmail.com>
2021-01-16 16:00:42 +0800

    personalized content

--- src/components/HelloDiv.jsx
+++ src/components/HelloDiv.jsx
@@ -3,12 +3,12 @@ import React from "react";
 function HelloDiv() {
   return (
     <div>
-      <h1>Hey guys! I'm a header!</h1>
-      <h2>Things I like!</h2>
+      <h1>Hello, my name is Irene Asay.</h1>
+      <h2>Languages I speak:</h2>
       <ul>
-        <li>Peas</li>
-        <li>Carrots</li>
-        <li>Love</li>
+        <li>English</li>
+        <li>Chinese</li>
+        <li>Taiwanese</li>
       </ul>
     </div>
   );
